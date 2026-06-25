c730f8d2d435544acc29211f7a1647c51694da84
Raghulvignesh-digital <Raghulpersonal07@gmail.com>
2026-06-25 11:21:47 +0100

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'thinkspend-v1';
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll(['./','./index.html'])
+    )
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
+
+self.addEventListener('message', e => {
+  if(e.data.action === 'skipWaiting') self.skipWaiting();
+});
