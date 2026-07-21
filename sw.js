4ffa8421b4d6bbadf2a418ed443f50ec03f55f94
Raghulvignesh-digital <Raghulpersonal07@gmail.com>
2026-07-21 18:23:29 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,26 @@
-const CACHE = 'thinkspend-v5';
+const CACHE = 'thinkspend-cache';
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache =>
-      cache.addAll(['./','./index.html'])
-    )
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    fetch(e.request)
+      .then(res => {
+        const resClone = res.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, resClone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
 
